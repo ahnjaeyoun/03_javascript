e7fde1171c341ec58f931e1eb3704d38707d366b
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-09-07 17:45:50 +0900

    2023 09 07 academy

---
+++ js/penguin.js
@@ -0,0 +1,6 @@
+document.getElementById("penguin").addEventListener("keydown", function() {
+   const div = document.createElement("div");
+   const img = document.createElement("img src(/images/penguin.png)");
+
+});
+
